d853b7a6351237246fa3e77f8d0ca944a8127ec4
dlewando <klop1280@gmail.com>
2018-09-21 22:35:22 +0300

    RELOAD

---
+++ CoreWar/cmake-build-debug/CMakeFiles/corewar.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/dlewando/Corewar/corewar/CoreWar/src/commands/commands1-5.c" "/Users/dlewando/Corewar/corewar/CoreWar/cmake-build-debug/CMakeFiles/corewar.dir/src/commands/commands1-5.c.o"
+  "/Users/dlewando/Corewar/corewar/CoreWar/src/commands/commands11-15.c" "/Users/dlewando/Corewar/corewar/CoreWar/cmake-build-debug/CMakeFiles/corewar.dir/src/commands/commands11-15.c.o"
+  "/Users/dlewando/Corewar/corewar/CoreWar/src/commands/commands6-10.c" "/Users/dlewando/Corewar/corewar/CoreWar/cmake-build-debug/CMakeFiles/corewar.dir/src/commands/commands6-10.c.o"
+  "/Users/dlewando/Corewar/corewar/CoreWar/src/main.c" "/Users/dlewando/Corewar/corewar/CoreWar/cmake-build-debug/CMakeFiles/corewar.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib/mylib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/dlewando/Corewar/corewar/CoreWar/cmake-build-debug/lib/mylib/CMakeFiles/mylib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CoreWar/cmake-build-debug/CMakeFiles/corewar.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/corewar.dir/src/main.c.o"
+  "CMakeFiles/corewar.dir/src/commands/commands1-5.c.o"
+  "CMakeFiles/corewar.dir/src/commands/commands6-10.c.o"
+  "CMakeFiles/corewar.dir/src/commands/commands11-15.c.o"
+  "corewar.pdb"
+  "corewar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/corewar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
